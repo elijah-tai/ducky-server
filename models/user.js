fa4048062b84aeb7811ecf08b5e3899627cbbf23
WonJune Tai <wonjune.tai@gmail.com>
2015-11-02 20:51:43 -0500

    Quick add for convo and request schemas to users

--- models/user.js
+++ models/user.js
@@ -1,14 +1,16 @@
 'use strict'
 
 var mongoose = require('mongoose'),
+		Request = require('./request'),
+		Conversation = require('./conversation'),
 		Schema = mongoose.Schema
 
 var User = new Schema({
 	d: String, // device ID
 	u: String, // username
 	p: {
-		r: [String], // IDs of requests
-		c: [Array] // IDs of conversations
+		r: [Request.schema], // IDs of requests
+		c: [Conversation.schema] // IDs of conversations
 	}
 })
 
